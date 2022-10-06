30c5301c23b8d65c42252ffccf8b41030ca4ee7b
Elie Pouget <elie.pouget@etudiant.univ-reims.fr>
2022-10-06 11:12:00 +0200

    Ajout de condition sur les paramètres manyTimes

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -16,15 +16,19 @@ class HelloController extends AbstractController
         ]);
     }
 
-    #[Route('/hello/{name}')]
+    #[Route('/hello/{name}/{time}', name: 'app_hello_manytimes', requirements: ['time' => '\d+'])]
+    public function manyTimes(string $name, int $time = 3)
+    {
+        if (!($time > 0 && $time <= 10)) {
+            $time = 3;
+        }
+
+        return $this->render('hello/many_times.html.twig', ['name' => $name, 'time' => $time]);
+    }
+
+    #[Route('/hello/{name}', name: 'app_hello_world')]
     public function world(string $name): Response
     {
         return $this->render('hello/world.html.twig', ['name' => $name]);
-    }
-
-    #[Route('/hello/{name}/{time}', name: 'app_hello_manytimes')]
-    public function manyTimes(string $name, int $time)
-    {
-        return $this->render('hello/many_times.html.twig', ['name' => $name, 'time' => $time]);
     }
 }
