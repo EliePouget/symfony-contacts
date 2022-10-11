c6a94eddbee700ab95e8e7b55271f738e3625d6b
Elie Pouget <elie.pouget@etudiant.univ-reims.fr>
2022-10-11 11:05:07 +0200

    Modification méthode getDefaults

--- src/Factory/ContactFactory.php
+++ src/Factory/ContactFactory.php
@@ -4,28 +4,28 @@ namespace App\Factory;
 
 use App\Entity\Contact;
 use App\Repository\ContactRepository;
-use Zenstruck\Foundry\RepositoryProxy;
 use Zenstruck\Foundry\ModelFactory;
 use Zenstruck\Foundry\Proxy;
+use Zenstruck\Foundry\RepositoryProxy;
 
 /**
  * @extends ModelFactory<Contact>
  *
- * @method static Contact|Proxy createOne(array $attributes = [])
- * @method static Contact[]|Proxy[] createMany(int $number, array|callable $attributes = [])
- * @method static Contact[]|Proxy[] createSequence(array|callable $sequence)
- * @method static Contact|Proxy find(object|array|mixed $criteria)
- * @method static Contact|Proxy findOrCreate(array $attributes)
- * @method static Contact|Proxy first(string $sortedField = 'id')
- * @method static Contact|Proxy last(string $sortedField = 'id')
- * @method static Contact|Proxy random(array $attributes = [])
- * @method static Contact|Proxy randomOrCreate(array $attributes = [])
- * @method static Contact[]|Proxy[] all()
- * @method static Contact[]|Proxy[] findBy(array $attributes)
- * @method static Contact[]|Proxy[] randomSet(int $number, array $attributes = [])
- * @method static Contact[]|Proxy[] randomRange(int $min, int $max, array $attributes = [])
+ * @method static Contact|Proxy                     createOne(array $attributes = [])
+ * @method static Contact[]|Proxy[]                 createMany(int $number, array|callable $attributes = [])
+ * @method static Contact[]|Proxy[]                 createSequence(array|callable $sequence)
+ * @method static Contact|Proxy                     find(object|array|mixed $criteria)
+ * @method static Contact|Proxy                     findOrCreate(array $attributes)
+ * @method static Contact|Proxy                     first(string $sortedField = 'id')
+ * @method static Contact|Proxy                     last(string $sortedField = 'id')
+ * @method static Contact|Proxy                     random(array $attributes = [])
+ * @method static Contact|Proxy                     randomOrCreate(array $attributes = [])
+ * @method static Contact[]|Proxy[]                 all()
+ * @method static Contact[]|Proxy[]                 findBy(array $attributes)
+ * @method static Contact[]|Proxy[]                 randomSet(int $number, array $attributes = [])
+ * @method static Contact[]|Proxy[]                 randomRange(int $min, int $max, array $attributes = [])
  * @method static ContactRepository|RepositoryProxy repository()
- * @method Contact|Proxy create(array|callable $attributes = [])
+ * @method        Contact|Proxy                     create(array|callable $attributes = [])
  */
 final class ContactFactory extends ModelFactory
 {
@@ -38,11 +38,16 @@ final class ContactFactory extends ModelFactory
 
     protected function getDefaults(): array
     {
+        $firstname = self::faker()->firstName();
+        $lastname = self::faker()->lastName();
+        $email = transliterator_transliterate(
+            'Any-Latin; Latin-ASCII',
+            mb_strtolower($firstname.'.'.$lastname.'@'.self::faker()->domainName()));
         return [
             // TODO add your default values here (https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#model-factories)
-            'firstname' => self::faker()->text(),
-            'lastname' => self::faker()->text(),
-            'email' => self::faker()->text(),
+            'firstname' => $firstname,
+            'lastname' => $lastname,
+            'email' => $email,
         ];
     }
 
