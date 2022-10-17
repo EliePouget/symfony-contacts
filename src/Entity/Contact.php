91ff332ba02d48535939839fefdc0a6fa0cfcc64
Elie Pouget <elie.pouget@etudiant.univ-reims.fr>
2022-10-17 16:06:29 +0200

    Application de la migration et mise à jour de la forge

--- src/Entity/Contact.php
+++ src/Entity/Contact.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\ContactRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: ContactRepository::class)]
@@ -21,6 +23,17 @@ class Contact
 
     #[ORM\Column(length: 100)]
     private ?string $email = null;
+
+    #[ORM\Column(length: 20)]
+    private ?string $phone = null;
+
+    #[ORM\OneToMany(mappedBy: 'category', targetEntity: Category::class)]
+    private Collection $categories;
+
+    public function __construct()
+    {
+        $this->categories = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -59,6 +72,48 @@ class Contact
     public function setEmail(string $email): self
     {
         $this->email = $email;
+
+        return $this;
+    }
+
+    public function getPhone(): ?string
+    {
+        return $this->phone;
+    }
+
+    public function setPhone(string $phone): self
+    {
+        $this->phone = $phone;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Category>
+     */
+    public function getCategories(): Collection
+    {
+        return $this->categories;
+    }
+
+    public function addCategory(Category $category): self
+    {
+        if (!$this->categories->contains($category)) {
+            $this->categories->add($category);
+            $category->setCategory($this);
+        }
+
+        return $this;
+    }
+
+    public function removeCategory(Category $category): self
+    {
+        if ($this->categories->removeElement($category)) {
+            // set the owning side to null (unless already changed)
+            if ($category->getCategory() === $this) {
+                $category->setCategory(null);
+            }
+        }
 
         return $this;
     }
--- src/Factory/ContactFactory.php
+++ src/Factory/ContactFactory.php
@@ -49,6 +49,8 @@ final class ContactFactory extends ModelFactory
             'firstname' => $firstname,
             'lastname' => $lastname,
             'email' => $email,
+            'phone' => self::faker()->phoneNumber(),
+
         ];
     }
 
