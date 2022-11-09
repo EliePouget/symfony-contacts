eeb54547194a345df123d6e9aa983723b95e6755
Elie Pouget <elie.pouget@etudiant.univ-reims.fr>
2022-11-09 18:20:33 +0100

    Ajout entité catégories

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -13,8 +13,12 @@ class CategoryController extends AbstractController
     #[Route('/category', name: 'app_category')]
     public function index(CategoryRepository $categoryRepository): Response
     {
-        $categories = $categoryRepository->findAll();
-
+        $qb = $categoryRepository->createQueryBuilder('c')
+            ->select(['c as category'])
+            ->addSelect('COUNT(co) as count')
+            ->leftJoin('c.contacts', 'co')
+            ->orderBy('c.name', 'ASC')
+            ->groupBy('c');
         return $this->render('category/index.html.twig', [
             'listCategory' => $categories,
         ]);
